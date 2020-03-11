00f0797f7ba89e2173bedc799c81e7db719675cb
WalzDS <david-simon.walz@basf.com>
2020-03-11 22:07:57 +0100

    add test requires

--- setup.py
+++ setup.py
@@ -7,11 +7,12 @@ setup(
     name="sobol",
     version="0.1",
     author="David Walz",
-    description="Pythonic implementation of the Sobol sequence.",
+    description="Sobol sequence for low-discrepency quasi-random numbers",
     long_description=long_description,
     long_description_content_type="text/markdown",
     url="https://github.com/DavidWalz/sobol",
     packages=find_packages(),
     python_requires=">=3.5",
     install_requires=["numpy"],
+    test_require=["pytest", "torch"],
 )
