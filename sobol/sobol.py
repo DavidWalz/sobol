5bbe7159def67601cb54cd42609f415ae997ebe7
WalzDS <david-simon.walz@basf.com>
2020-03-11 14:54:04 +0100

    use direction numbers for up to 1111 dimensions

--- sobol/sobol.py
+++ sobol/sobol.py
@@ -1,56 +1,4 @@
 import numpy as np
-from io import StringIO
-
-DIM_MAX = 40  # maximum number of dimensions
-LOG_MAX = 30  # maximum number of bits
-POLY = np.genfromtxt(
-    StringIO(
-        """# POLY, and first 8 columns of V
-        1, 1,  0,  0,  0,  0,  0,  0,  0
-        3, 1,  0,  0,  0,  0,  0,  0,  0
-        7, 1,  1,  0,  0,  0,  0,  0,  0
-        11, 1,  3,  7,  0,  0,  0,  0,  0
-        13, 1,  1,  5,  0,  0,  0,  0,  0
-        19, 1,  3,  1,  1,  0,  0,  0,  0
-        25, 1,  1,  3,  7,  0,  0,  0,  0
-        37, 1,  3,  3,  9,  9,  0,  0,  0
-        59, 1,  3,  7, 13,  3,  0,  0,  0
-        47, 1,  1,  5, 11, 27,  0,  0,  0
-        61, 1,  3,  5,  1, 15,  0,  0,  0
-        55, 1,  1,  7,  3, 29,  0,  0,  0
-        41, 1,  3,  7,  7, 21,  0,  0,  0
-        67, 1,  1,  1,  9, 23, 37,  0,  0
-        97, 1,  3,  3,  5, 19, 33,  0,  0
-        91, 1,  1,  3, 13, 11,  7,  0,  0
-        109, 1,  1,  7, 13, 25,  5,  0,  0
-        103, 1,  3,  5, 11,  7, 11,  0,  0
-        115, 1,  1,  1,  3, 13, 39,  0,  0
-        131, 1,  3,  1, 15, 17, 63, 13,  0
-        193, 1,  1,  5,  5,  1, 27, 33,  0
-        137, 1,  3,  3,  3, 25, 17,115,  0
-        145, 1,  1,  3, 15, 29, 15, 41,  0
-        143, 1,  3,  1,  7,  3, 23, 79,  0
-        241, 1,  3,  7,  9, 31, 29, 17,  0
-        157, 1,  1,  5, 13, 11,  3, 29,  0
-        185, 1,  3,  1,  9,  5, 21,119,  0
-        167, 1,  1,  3,  1, 23, 13, 75,  0
-        229, 1,  3,  3, 11, 27, 31, 73,  0
-        171, 1,  1,  7,  7, 19, 25,105,  0
-        213, 1,  3,  5,  5, 21,  9,  7,  0
-        191, 1,  1,  1, 15,  5, 49, 59,  0
-        253, 1,  1,  1,  1,  1, 33, 65,  0
-        203, 1,  3,  5, 15, 17, 19, 21,  0
-        211, 1,  1,  7, 11, 13, 29,  3,  0
-        239, 1,  3,  7,  5,  7, 11,113,  0
-        247, 1,  1,  5,  3, 15, 19, 61,  0
-        285, 1,  3,  1,  1,  9, 27, 89,  7
-        369, 1,  1,  3,  7, 31, 15, 45, 23
-        299, 1,  3,  3,  9,  9, 25,107, 39
-        """
-    ),
-    delimiter=",",
-    dtype=int,
-)
 
 
 def bit_lo0(n):
@@ -64,24 +12,29 @@ def bit_lo0(n):
 
 def generator(n_dim):
     """Generator for the Sobol sequence"""
-    poly = POLY[:, 0]
-    V = np.zeros((DIM_MAX, LOG_MAX), dtype=int)
-    V[:, :8] = POLY[:, 1:]
+    DIMS = 1111  # maximum number of dimensions
+    BITS = 30  # maximum number of bits
+
+    # initialize direction numbers
+    V = np.zeros((DIMS, BITS), dtype=int)
+    data = np.genfromtxt(__file__.replace(".py", "1111.tsv"), dtype=int)
+    poly = data[:, 0]
+    V[:, :13] = data[:, 1:14]
     V[0, :] = 1
     for i in range(1, n_dim):
         m = len(np.binary_repr(poly[i])) - 1
         include = np.array([int(b) for b in np.binary_repr(poly[i])[1:]])
-        for j in range(m, LOG_MAX):
+        for j in range(m, BITS):
             V[i, j] = V[i, j - m]
             for k in range(m):
                 if include[k]:
                     V[i, j] = np.bitwise_xor(V[i, j], 2 ** (k + 1) * V[i, j - k - 1])
-    V = V[:n_dim] * 2 ** np.arange(LOG_MAX)[::-1]
+    V = V[:n_dim] * 2 ** np.arange(BITS)[::-1]
 
     point = np.zeros(n_dim, dtype=int)
-    for i in range(2 ** LOG_MAX):
+    for i in range(2 ** BITS):
         point = np.bitwise_xor(point, V[:, bit_lo0(i)])
-        yield point / 2 ** LOG_MAX
+        yield point / 2 ** BITS
 
 
 def sample(n_dim, n_points, skip=0):
@@ -94,17 +47,15 @@ def sample(n_dim, n_points, skip=0):
     n_points : int, optional
         Number of points to sample
     skip : int, optional
-        Number of points in the sequence to skip, by default 1
+        Number of points in the sequence to skip, by default 0
 
     Returns
     -------
     array, shape=(n_samples, n_dim)
         Samples from the Sobol sequence.
     """
-    if not (1 <= n_dim <= DIM_MAX):
-        raise ValueError("Sobol: n_dim must be between 1 and 40.")
-    if n_points + skip >= 2 ** LOG_MAX:
-        raise ValueError(f"Sobol: n_points must be < 2**{LOG_MAX} including skip")
+    if not (1 <= n_dim <= 1111):
+        raise ValueError("Sobol: n_dim must be between 1 and 1111.")
 
     sobol = generator(n_dim)
     for i in range(skip):
